0942a497c17619148ce38c8c49c5d4445777a8e6
chatis <chatis@becurity.com>
2019-05-05 00:14:48 -0500

    added test for todo component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,17 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {TodoComponent} from './components/todo/todo.component';
+import {FormsModule} from '@angular/forms';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule, FormsModule
       ],
       declarations: [
-        AppComponent
+        AppComponent, TodoComponent
       ],
     }).compileComponents();
   }));
@@ -18,18 +20,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'todo-angular'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('todo-angular');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to todo-angular!');
   });
 });
--- src/app/components/todo/todo.component.spec.ts
+++ src/app/components/todo/todo.component.spec.ts
@@ -1,6 +1,10 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  async, ComponentFixture, TestBed,
+} from '@angular/core/testing';
 
 import { TodoComponent } from './todo.component';
+import {FormsModule} from '@angular/forms';
+import {until} from 'selenium-webdriver';
 
 describe('TodoComponent', () => {
   let component: TodoComponent;
@@ -8,7 +12,12 @@ describe('TodoComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TodoComponent ]
+      imports: [
+        FormsModule
+      ],
+      declarations: [
+        TodoComponent
+      ]
     })
     .compileComponents();
   }));
@@ -16,10 +25,42 @@ describe('TodoComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TodoComponent);
     component = fixture.componentInstance;
+    component.todo = {
+      what: 'this is a task',
+      isDone: false
+    };
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should display todo\'s what in textarea when todo is not done', () => {
+    setTimeout(() => {
+      expect(component.editor.nativeElement.value).toContain(component.todo.what);
+    }, 200);
+  });
+  it('should show done div when todo is done', () => {
+    const componentNative = fixture.debugElement.nativeElement;
+    component.todo.isDone = true;
+    fixture.detectChanges();
+    expect(componentNative.querySelector('#done')).toBeTruthy();
+  });
+  it('should hide not done div when todo is done', () => {
+    const componentNative = fixture.debugElement.nativeElement;
+    component.todo.isDone = true;
+    fixture.detectChanges();
+    expect(componentNative.querySelector('#not-done')).toBe(null);
+  });
+  it('should show not done div when todo is not done', () => {
+    const componentNative = fixture.debugElement.nativeElement;
+    fixture.detectChanges();
+    expect(componentNative.querySelector('#not-done')).toBeTruthy();
+  });
+  it('should hide done div when todo is not done', () => {
+    const componentNative = fixture.debugElement.nativeElement;
+    fixture.detectChanges();
+    expect(componentNative.querySelector('#done')).toBe(null);
+  });
+
 });
